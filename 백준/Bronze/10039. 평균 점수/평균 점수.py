2bc3cc2e3a82b525218ae3e4cd03c1f8a90e6392
Lee Eungyo <ialice119@naver.com>
2026-02-27 01:52:06 +0900

    [Bronze IV] Title: 평균 점수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10039. 평균 점수/평균 점수.py
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -1,9 +1,8 @@
 total=0
 for i in range(5):
     score=int(input())
-    if score<40:
+    if(score<40):
         score=40
-    total=total+score
+    total+=score    
 
-result=total//5
-print(result)
+print(total//5)
