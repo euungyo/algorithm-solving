bab3c9221e3f4c5e6abb8dff3fd975233a72f6ed
Lee Eungyo <ialice119@naver.com>
2026-02-28 05:46:35 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,34 +1,9 @@
-a=list(input())
-for i in range(len(a),15):
-    a.append('')
-b=list(input())
-for i in range(len(b),15):
-    b.append('')
-c=list(input())
-for i in range(len(c),15):
-    c.append('')
-d=list(input())
-for i in range(len(d),15):
-    d.append('')
-e=list(input())
-for i in range(len(e),15):
-    e.append('')
+strings=list(input() for i in range(5))
 ans=[]
 
 for i in range(15):
-    if(a[i]=='' and b[i]=='' and 
-       c[i]=='' and d[i]=='' and e[i]==''):
-        break
-
-    if(a[i]!=''):
-        ans.append(a[i])    
-    if(b[i]!=''):
-        ans.append(b[i])    
-    if(c[i]!=''):
-        ans.append(c[i])    
-    if(d[i]!=''):
-        ans.append(d[i])    
-    if(e[i]!=''):
-        ans.append(e[i])
+    for j in strings:
+        if(i<len(j)):
+            ans.append(j[i])
 
 print(''.join(ans))
