257d89b74220603c828b4ccc4324976b76db05f7
Lee Eungyo <ialice119@naver.com>
2026-02-28 13:50:24 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 72 ms, Memory: 36480 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -1,12 +1,13 @@
 li=[]
-
-for i in range(1,1001):
-    for j in range(i):
-        li.append(i)
-
 a,b=map(int,input().split())
 count=0
 
+for i in range(1,b+1):
+    for j in range(i):
+        li.append(i)
+
+
 for i in range(a-1,b):
     count+=li[i]
+
 print(count)    
