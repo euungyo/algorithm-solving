dc369a40e9d641c859fe7496dde9349fa1f77649
Lee Eungyo <ialice119@naver.com>
2026-03-04 15:56:40 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+n=int(input())
+result=0
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(n):
+    result+=a[i]*b[i]
+
+print(result)
