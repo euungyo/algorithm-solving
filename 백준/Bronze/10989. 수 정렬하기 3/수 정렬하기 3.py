292eb1d4fa54af1eccff06f868eb116192bb553f
Lee Eungyo <ialice119@naver.com>
2026-03-13 22:17:36 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 7848 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+count=[0]*10001
+
+for i in range(n):
+    count[int(input())]+=1
+
+for i in range(10001):
+    for j in range(count[i]):
+        print(i)
