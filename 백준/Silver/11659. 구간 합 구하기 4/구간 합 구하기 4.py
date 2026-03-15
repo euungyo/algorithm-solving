0fcf48d591b537bdc05e2420ece7271b37186f3b
Lee Eungyo <ialice119@naver.com>
2026-03-15 19:02:31 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 244 ms, Memory: 41144 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+li=list(map(int,input().split()))
+
+sum=[0]*(n+1)
+
+for i in range(1,n+1):
+    sum[i]=sum[i-1]+li[i-1]
+
+for k in range(m):
+    i,j=map(int,input().split())
+    print(sum[j]-sum[i-1])
