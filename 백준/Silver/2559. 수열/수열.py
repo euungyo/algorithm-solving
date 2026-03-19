457bc686009bdda0e95df362aa375b53a9341c8d
Lee Eungyo <ialice119@naver.com>
2026-03-19 23:51:03 +0900

    [Silver III] Title: 수열, Time: 80 ms, Memory: 41632 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+n,k=map(int,input().split())
+nums=list(map(int,input().split()))
+
+prefix=[0]*(n+1)
+
+for i in range(n):
+    prefix[i+1]=prefix[i]+nums[i]
+
+result=[]
+for i in range(n+1-k):
+    result.append(prefix[k+i]-prefix[i])
+
+print(max(result))
